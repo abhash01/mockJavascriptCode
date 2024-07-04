c25f452bf7c8e95dbe120880fce12684bedc9874
abhash <abhash01@gmail.com>
2024-07-04 16:26:57 +0530

    rough work

---
+++ practice.js
@@ -0,0 +1,67 @@
+//1. Checking Sum zero
+//[-5,-4,-3,-2,0,2,4,6,8] -> inpout
+// [-4,4] -> output
+//rough task
+
+//Check if the user with such name Exits;
+const user = [
+  {
+    id: 1,
+    name: "abhash",
+    isActive: true,
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "test1",
+    isActive: true,
+    age: 19,
+  },
+  {
+    id: 3,
+    name: "test2",
+    isActive: true,
+    age: 26,
+  },
+  {
+    id: 4,
+    name: "test3",
+    isActive: false,
+    age: 28,
+  },
+];
+
+function isUserExist(name, user) {
+  let exits = false;
+  for (let i = 0; i < user.length; i++) {
+    if (user[i].name === name) {
+      exits = true;
+    }
+  }
+  return exits;
+  //if(user)
+}
+
+//const res = isUserExist("test4",user);
+//console.log(res)
+// pbl 2 solution
+
+const isNameExits = (name, users) => {
+  const user = users.find((user) => user.name === name);
+  return Boolean(user);
+};
+console.log(isNameExits("test2", user));
+
+// pbl 3 solution
+const isNameExt = (name, user) => {
+  const result = user.findIndex((user) => user.name === name);
+  return result >= 0;
+};
+console.log(isNameExt("test2", user));
+
+// pbl 4 solution
+const isNameExitSome = (name, users) => {
+  const user = users.some((user) => user.name === name);
+  return user;
+};
+console.log(isNameExitSome("test4", user));
