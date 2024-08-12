e7ee6a219f949e370b7923aec5e44e5b3dfaf2e2
abhash <abhash01@gmail.com>
2024-08-12 23:40:57 +0530

    little change

--- leet.js
+++ leet.js
@@ -48,6 +48,7 @@ var removeElement = function (nums, val) {
   return index;
 };
 
+//Solution
 // 35. Search Insert Position
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
