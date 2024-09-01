5985c7d5bb075048bf15fca91e41cdbc0825156a
abhash <abhash01@gmail.com>
2024-09-01 13:14:56 +0530

    uncomment

--- codestepBystep.js
+++ codestepBystep.js
@@ -102,29 +102,29 @@
 // 12
 
 // 13.
-// let number = 0;
-// console.log(number++); // => 0
-// console.log(++number); // => 2
-// console.log(number); // => 2
+let number = 0;
+console.log(number++); // => 0
+console.log(++number); // => 2
+console.log(number); // => 2
 
 // 14.
-// const obj = { 1: "a", 2: "b", 3: "c" };
+const obj = { 1: "a", 2: "b", 3: "c" };
 
-// console.log(obj.hasOwnProperty("1")); //=> true
-// console.log(obj.hasOwnProperty(1)); //=> true
+console.log(obj.hasOwnProperty("1")); //=> true
+console.log(obj.hasOwnProperty(1)); //=> true
 
 // 15.
-// const obj = { a: "one", b: "two", a: "three" };
-// console.log(obj);
+const obj = { a: "one", b: "two", a: "three" };
+console.log(obj);
 
 //ANS
 // {a: 'three', b: 'two'}  => position remain same but value override
 
 // 16.
-// for (let i = 1; i < 5; i++) {
-//   if (i === 3) continue;
-//   console.log(i);
-// }
+for (let i = 1; i < 5; i++) {
+  if (i === 3) continue;
+  console.log(i);
+}
 
 //ANS
 // 1
@@ -132,14 +132,14 @@
 // 4
 
 // 17.
-// const foo = () => console.log("First");
-// const bar = () => setTimeout(() => console.log("second"));
-// const baz = () => console.log("third");
+const foo = () => console.log("First");
+const bar = () => setTimeout(() => console.log("second"));
+const baz = () => console.log("third");
 
-// bar();
-// foo();
-// baz();
-// function declaration is noit important function call is important
+bar();
+foo();
+baz();
+//function declaration is noit important function call is important
 //ANS
 // First;
 // third;
@@ -147,13 +147,13 @@
 
 // 18.
 
-// const person = { name: "abhash" };
-// function sayHi(age) {
-//   return `${this.name} is ${age}`;
-// }
+const person = { name: "abhash" };
+function sayHi(age) {
+  return `${this.name} is ${age}`;
+}
 
-// console.log(sayHi.call(person, 21));
-// console.log(sayHi.bind(person, 21)); //=> it return fn , to print call the fn() console.log(sayHi.bind(person, 21)())
+console.log(sayHi.call(person, 21));
+console.log(sayHi.bind(person, 21)); //=> it return fn , to print call the fn() console.log(sayHi.bind(person, 21)())
 
 //ANS
 // abhash is 21
@@ -175,22 +175,22 @@
 //number //=>  becoz arrow function call immediate after retun
 
 // 20.
-// console.log(typeof 1); // number
-// console.log(typeof typeof 1); // string
+console.log(typeof 1); // number
+console.log(typeof typeof 1); // string
 
 // 21.
-// console.log(!!null); //=> false
-// console.log(!!""); //=> false
-// console.log(!!1); //=> true
+console.log(!!null); //=> false
+console.log(!!""); //=> false
+console.log(!!1); //=> true
 
 // 22.
-// console.log([..."abhash"]); //=> ['a', 'b', 'h', 'a', 's', 'h']
-// console.log([...[1, 2, 3, 5]]); //=> [1, 2, 3, 5]
+console.log([..."abhash"]); //=> ['a', 'b', 'h', 'a', 's', 'h']
+console.log([...[1, 2, 3, 5]]); //=> [1, 2, 3, 5]
 
 // 23.
-// let data = 3 + 4 + "5";
-// console.log(typeof data);  //=> String
-// console.log(typeof 3 + 4 + "5"); //=> number45
-// console.log(typeof (3 + 4 + +"5")) //=> number bcoz we add plus symbol before string 5
-// console.log(typeof []) //=> object;
-// console.log([] == []) //=> false bcoz both using the different memory location.
+let data = 3 + 4 + "5";
+console.log(typeof data); //=> String
+console.log(typeof 3 + 4 + "5"); //=> number45
+console.log(typeof (3 + 4 + +"5")); //=> number bcoz we add plus symbol before string 5
+console.log(typeof []); //=> object;
+console.log([] == []); //=> false bcoz both using the different memory location.
