167ffe162ddf92233b30defee3426503ab6c57af
abhash <abhash01@gmail.com>
2024-09-16 23:47:04 +0530

    majorityElement

--- leet.js
+++ leet.js
@@ -363,3 +363,36 @@ var search = function (nums, target) {
   }
   return -1;
 };
+
+//169. Majority Element
+//Given an array nums of size n, return the majority element.
+
+//The majority element is the element that appears more than ⌊n / 2⌋ times.
+//You may assume that the majority element always exists in the array.
+//Input: nums = [2,2,1,1,1,2,2] Output: 2
+//Input: nums = [3,2,3] Output: 3
+
+var majorityElement = function (nums) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const freq = (map.get(nums[i]) ?? 0) + 1;
+    map.set(nums[i], freq);
+    //console.log(map)
+    //Map(2) { 2 => 4, 1 => 3 }
+    if (freq > nums.length / 2) return nums[i];
+  }
+  return nums[0];
+};
+
+// this is also possible
+// var majorityElement = function(nums) {
+//     let obj = {}
+//     for(let i = 0;i<nums.length;i++){
+//         obj[nums[i]] = (obj[nums[i]] || 0) + 1
+//     }
+//     const maxValue = Math.max(...Object.values(obj));
+//     const key = Object.keys(obj).find((key) => {
+//         return obj[key] === maxValue
+//     })
+//     return key
+// };
