9e2cd89fcae8cafcc031f516a6c7479275ee3a1a
abhash <abhash01@gmail.com>
2024-09-18 23:53:27 +0530

    isPalindromeres

--- leet.js
+++ leet.js
@@ -593,3 +593,33 @@ console.log(lengword);
 //     const lastword = s.trim().split(" ")
 //     return (lastword[lastword.length-1].length)
 // };
+
+// 125. Valid Palindrome
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+// Input: s = "A man, a plan, a canal: Panama" Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Example 2:
+// Input: s = "race a car" Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// Example 3:
+// Input: s = " " Output: true
+
+function isPalindrome(str) {
+  let newstr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  let left = 0;
+  let right = newstr.length - 1;
+  while (left <= right) {
+    if (newstr[left] !== newstr[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+const isPalindromeres = isPalindrome("A man, a plan, a canal: Panama");
+console.log(isPalindromeres);
