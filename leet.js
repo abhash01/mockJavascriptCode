43884d7d9555928df1389b101d581b64e8e05353
abhash <abhash01@gmail.com>
2024-09-19 12:20:38 +0530

    isPalimdrome

--- leet.js
+++ leet.js
@@ -731,3 +731,34 @@ function twoSum(nums, target) {
 
 const twoSumres = twoSum([2, 7, 11, 15], 9);
 console.log(twoSumres);
+
+// 9. Palindrome Number
+// Given an integer x, return true if x is a
+// palindrome  and false otherwise.
+
+// Example 1:
+// Input: x = 121 Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+// Example 2:
+// Input: x = -121 Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+// Example 3:
+// Input: x = 10 Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+function isPalindrome(nums) {
+  const newnums = nums.toString();
+  let left = 0,
+    right = newnums.length - 1;
+  while (left <= right) {
+    if (newnums[left] !== newnums[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
+
+const resultisPalindrome = isPalindrome(-121);
+console.log(resultisPalindrome);
