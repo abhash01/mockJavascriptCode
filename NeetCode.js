9a1fd0ec14de954e0fcca0beb328b0272160d204
Abhash kumar ranjan <32848595+abhash01@users.noreply.github.com>
2025-02-05 22:19:53 +0530

    add file

--- NeetCode.js
+++ NeetCode.js
@@ -119,3 +119,33 @@ function bestTime(arr) {
 
 const resStock = bestTime([10, 1, 5, 6, 7, 1]);
 console.log(resStock);
+
+//6 //Valid Parentheses
+//You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+//The input string s is valid if and only if:
+
+//Every open bracket is closed by the same type of close bracket.
+//Open brackets are closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+//Return true if s is a valid string, and false otherwise.
+
+//Input: s = "[]" Output: true
+//Input: s = "([{}])" Output: true
+//Input: s = "[(])" Output: false
+
+function isValidPar(str) {
+  let stack = [];
+  for (let item of str) {
+    if (item === "(") stack.push(")");
+    else if (item === "[") stack.push("]");
+    else if (item === "{") stack.push("}");
+    else if (item !== stack.pop()) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+const resIsValid = isValidPar("[]");
+console.log(resIsValid);
